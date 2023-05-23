8cf86ed9623ce7842ec35efdb75006d1e299fae9
Brendon Angelo <brendonangelo@hotmail.com>
2023-05-23 04:50:39 -0300

    chore: api response time changed

--- miragejs/routes/index.js
+++ miragejs/routes/index.js
@@ -1,11 +1,10 @@
 /*
  * Mirage JS guide on Routes: https://miragejs.com/docs/route-handlers/functions
  */
-
 // import { Response } from 'miragejs';
 export default function routes() {
   this.namespace = 'api';
-  this.timing = 5000;
+  this.timing = 3000;
 
   /*
    * A resource comprises all operations for a CRUD
@@ -16,6 +15,7 @@ export default function routes() {
   this.resource('products');
 
   // ---------Force api return error---------
+  // Para simular um erro comente this.resource('products') e descomente a importação do Response
 
   // this.get('products', () => {
   //   return new Response(500, {}, 'O server morreu!');
