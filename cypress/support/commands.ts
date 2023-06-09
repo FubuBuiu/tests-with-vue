f0410b7aa73e4353c0a2d77a8b2f5d420757c08d
Brendon Angelo <brendonangelo@hotmail.com>
2023-06-09 04:27:24 -0300

    chore: adding new Cypress commands

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -8,6 +8,43 @@
 // commands please read more here:
 // https://on.cypress.io/custom-commands
 // ***********************************************
+Cypress.Commands.add('getByTestId', (selector) => {
+  return cy.get(`[data-testid="${selector}"]`);
+});
+
+Cypress.Commands.add('addToCart', (mode) => {
+  cy.getByTestId('productCard').as('productCards');
+
+  function addByIndex(index: number) {
+    cy.get('@productCards').eq(index).find('button').click();
+  }
+  function addByIndexes(indexesList: Array<number>) {
+    for (const index of indexesList) {
+      cy.get('@productCards').eq(index).find('button').click({ force: true });
+    }
+  }
+  function addAll() {
+    cy.get('@productCards').then(($elements) => {
+      let index = 0;
+      while (index < $elements.length) {
+        cy.get('@productCards').eq(index).find('button').click({ force: true });
+        index++;
+      }
+    });
+  }
+
+  if (Array.isArray(mode)) {
+    addByIndexes(mode);
+  } else if (typeof mode === 'number') {
+    addByIndex(mode);
+  } else if (typeof mode === 'string' && mode === 'all') {
+    addAll();
+  } else {
+    throw new Error(
+      'Please provide a valid input for cy.addToCart()\r\nProssible values are Array, number or "all"'
+    );
+  }
+});
 //
 //
 // -- This is a parent command --
